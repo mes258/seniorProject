c33a5971764c766446cb82048a3011a00c862f2b
mes258 <michael.smith.singapore@gmail.com>
2019-10-10 13:59:05 -0400

    Added simple log in app that doesn't store the username

---
+++ mixNMatch/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'welcome/index'
+  #get "log_out" => "sessions#destroy", :as => "log_out"
+  #get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
+  #resources :sessions
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #root 'welcome#index'
+end
