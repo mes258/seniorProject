1bfdeb213ce5e0fb9303c528efe68588bee205fb
mes258 <michael.smith.singapore@gmail.com>
2019-10-27 15:50:22 -0400

    added log in page

--- mixNMatch/config/routes.rb
+++ mixNMatch/config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'welcome/index'
-  #get "log_out" => "sessions#destroy", :as => "log_out"
-  #get "log_in" => "sessions#new", :as => "log_in"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
   root :to => "users#new"
   resources :users
-  #resources :sessions
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root 'welcome#index'
 end
