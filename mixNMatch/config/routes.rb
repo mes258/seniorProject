bacba13a7311a1c774ae87a24f533dad82ebdc1e
mes258 <michael.smith.singapore@gmail.com>
2019-11-16 15:46:53 -0500

    profile page link works, matches page now shows names, link to create new matches works

--- mixNMatch/config/routes.rb
+++ mixNMatch/config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   resources :profiles
   get 'sessions/new'
   get 'users/new'
-  get 'users/show'
   get 'welcome/index'
   get 'matches/index'
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  get "users/:id" => "users#show", :as => "show_user"
+  root :to => "sessions#new"
   resources :matches
   resources :users
   resources :sessions
