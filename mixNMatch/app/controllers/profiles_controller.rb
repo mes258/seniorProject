c4f57357c133b48c0fc3bdf1cb6ef444d9010de5
mes258 <michael.smith.singapore@gmail.com>
2019-12-01 22:04:48 -0500

    Users can now edit their ages

--- mixNMatch/app/controllers/profiles_controller.rb
+++ mixNMatch/app/controllers/profiles_controller.rb
@@ -163,6 +163,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:user_id, :first, :last, :pictureID, :picture, :description, :song, :preference, :gender, :value, :priority)
+      params.require(:profile).permit(:user_id, :first, :last, :pictureID, :picture, :description, :song, :preference, :gender, :value, :priority, :age)
     end
 end
