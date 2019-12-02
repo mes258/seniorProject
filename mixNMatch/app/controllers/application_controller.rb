bd28cdb0829ed1b5d180264bc51d4bf4edf4217c
mes258 <michael.smith.singapore@gmail.com>
2019-12-01 21:34:03 -0500

    Finished currency changes

--- mixNMatch/app/controllers/application_controller.rb
+++ mixNMatch/app/controllers/application_controller.rb
@@ -14,25 +14,32 @@ class ApplicationController < ActionController::Base
         @profile ||= Profile.find_by(user: session[:user_id]) if session[:user_id]
     end 
     
+    #update a user score when the match is made
     def update_score_match user
-        user[:score] = user[:score] - 2
-        puts("Updated score")
+        user[:score] = user[:score] - 5
         User.where(id: user[:id]).update_all(score: user[:score])
-        # @current_user.save
     end
 
+    #Update the user score when a match is accepted. 
     def update_score_match_accepted match_id
         match = Match.find_by(id: match_id)
+        scoreChange = 0
+        if(match.status1 == 1 && match.status2 == 1)
+            scoreChange = 20
+        elsif(match.status1 == 1 || match.status2 == 1)
+            scoreChange = 5
+        end
+
         userMatches = match.user_matches
-        puts("GETTING USERS")
         puts(userMatches)
 
         for current_user_match in userMatches
             current_u_id = current_user_match.user
             current_u = User.find_by(id: current_u_id)
-            new_score = current_u.score + 5; #this  works for a static amount. Need to change to work with the profile value
+            new_score = current_u.score + scoreChange
             User.where(id: current_u_id).update_all(score: new_score)
         end
+        
 
 
         # userMatch = User_Match.find_by(match: match_id)
