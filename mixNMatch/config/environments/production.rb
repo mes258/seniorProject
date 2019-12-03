57b3a8f793b0465770acc2ea533a9f4022756916
gjbsaiia <gjs64@case.edu>
2019-12-03 16:04:58 -0500

    secret key

--- mixNMatch/config/environments/production.rb
+++ mixNMatch/config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+	config.secret_key_base = '6969FinalSeniorProject6969'
+
 end
