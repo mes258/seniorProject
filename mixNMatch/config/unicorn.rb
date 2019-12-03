657154b078a3905c47b343f057461ab652273d1e
Ubuntu <ubuntu@ip-172-31-21-200.us-west-2.compute.internal>
2019-12-03 22:54:03 +0000

    mina configuration

--- mixNMatch/config/unicorn.rb
+++ mixNMatch/config/unicorn.rb
@@ -9,11 +9,11 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/tmp/sockets/unicorn.sock", :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
 stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+pid "#{shared_dir}/tmp/pids/unicorn.pid"
