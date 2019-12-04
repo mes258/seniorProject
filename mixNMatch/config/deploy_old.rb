b53ff826354159d7361ca265e9377406955ef623
gjbsaiia <gjs64@case.edu>
2019-12-03 21:37:48 -0500

    configuring server

--- mixNMatch/config/deploy_old.rb
+++ mixNMatch/config/deploy_old.rb
@@ -46,32 +46,11 @@ task :setup do
 
     # Create database.yml for Postgres if it doesn't exist
     path_database_yml = "config/database.yml"
-    database_yml = %[
-      default: &default
-        adapter: sqlite3
-        pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
-        timeout: 5000
-
-      development:
-        <<: *default
-        database: db/development.sqlite3
-
-      # Warning: The database defined as "test" will be erased and
-      # re-generated from your development database when you run "rake".
-      # Do not set this db to the same as development or production.
-      test:
-        <<: *default
-        database: db/test.sqlite3
-
-      production:
-        <<: *default
-        database: db/production.sqlite3]
-
-  #     production:
-  # database: #{fetch(:user)}
-  # adapter: postgresql
-  # pool: 5
-  # timeout: 5000
+    database_yml = %[production:
+  database: #{fetch(:user)}
+  adapter: postgresql
+  pool: 5
+  timeout: 5000]
     command %[test -e #{path_database_yml} || echo "#{database_yml}" > #{path_database_yml}]
 
     # Create secrets.yml if it doesn't exist
