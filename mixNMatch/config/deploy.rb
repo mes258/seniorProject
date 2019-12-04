e435e11a1f12e2d85a11335ba13bda5dd02f6265
gjbsaiia <gjs64@case.edu>
2019-12-03 22:29:34 -0500

    setup

--- mixNMatch/config/deploy.rb
+++ mixNMatch/config/deploy.rb
@@ -1,4 +1,5 @@
 require 'mina/rails'
+require 'mina/bundler'
 require 'mina/git'
 require 'mina/rvm'
 
@@ -89,7 +90,7 @@ task :deploy do
   end
 
   # you can use `run :local` to run tasks on local machine before of after the deploy scripts
-  # run(:local){ say 'done' }
+  run(:local){ say 'done' }
 end
 
 # For help in making your deploy script, see the Mina documentation:
