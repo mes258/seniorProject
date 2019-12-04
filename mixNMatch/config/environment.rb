7b63bc16b7e964ab19f34dee3e93d2646b5975b6
gjbsaiia <gjs64@case.edu>
2019-12-04 16:18:12 -0500

    passenger

--- mixNMatch/config/environment.rb
+++ mixNMatch/config/environment.rb
@@ -3,5 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# config.hosts << "ec2-34-221-97-48.us-west-2.compute.amazonaws.com"
--- mixNMatch/config/environments/development.rb
+++ mixNMatch/config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+	# Add AWS hostname
+	config.hosts << "ec2-34-221-97-48.us-west-2.compute.amazonaws.com"
+
 end
