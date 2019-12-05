2aaf409eb154930f68ba0fc067c04cc404d2f9c1
mes258 <michael.smith.singapore@gmail.com>
2019-12-05 13:31:07 -0500

    Fixed issue with accepting matches

--- mixNMatch/config/routes.rb
+++ mixNMatch/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'matches/index'
   get 'matches/update_status'
+  post 'matches/update_status'
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
