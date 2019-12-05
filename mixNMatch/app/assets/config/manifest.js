cbd23f05b15bbd4d72dbd8150800ea8dcd0fb35f
gjbsaiia <gjs64@case.edu>
2019-12-05 11:51:29 -0500

    assets manifest

--- mixNMatch/app/assets/config/manifest.js
+++ mixNMatch/app/assets/config/manifest.js
@@ -1,2 +1,9 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../javascripts .js
+
+//= require manifest
+//= require footer
+//= require icon
+//= require logo
+//= require middle
